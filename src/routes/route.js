45070ad7e6ef1989e4dec1d5fc9c16b8d1955af3
Anjali Srivastava <anjalisriv023@gmail.com>
2022-04-08 00:21:08 +0530

    assignment solution

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const res = require('express/lib/response');
 const logger = require('./logger')
 
 const router = express.Router();
@@ -15,6 +16,61 @@ router.get('/test-me', function (req, res) {
     console.log('------------------')
     console.log('These are the request query parameters: ', req.query)
     res.send('My first ever api!')
+});
+
+mov = ["rang de basanti", "the shining", "twinkle", "batman", "superman", "spiderman", "panda", "doggle", "vaspa", "stars"];
+  
+router.get('/movies', function(req, res){
+res.send(mov);
+}); 
+ 
+router.get('/movies1/:id', function(req, res){
+  for(let  i=0; i<=mov.length; i++){
+    if(req.params.id>=0 && req.params.id<mov.length){
+      console.log(mov[req.params.id])
+      res.send(mov[req.params.id]);
+    }else{
+      res.send("not valid index number");
+    }
+      
+      
+    }
+  
+});
+
+
+  movie = [ {
+    "id": 1,
+    "name": "The Shining"
+   }, {
+    "id": 2,
+    "name": "Incendies"
+   }, {
+    "id": 3,
+    "name": "Rang de Basanti"
+   }, {
+    "id": 4,
+    "name": "Finding Nemo"
+   }]
+   router.get('/films/:id1', function(req, res){
+    for( let i = 1; i<=movie.length; i++){
+        if(i==req.params.id1){
+        console.log(movie[i].name);
+            res.send(movie[i].name);
+
+        }
+    }
+});
+
+router.get('/films1/:filmId', function(req, res){
+  for(let i=0; i<=movie.length; i++){
+    if(i==req.params.filmId){
+      res.send(movie[i]);
+    }else {
+        res.send("No movie exists with this id");
+    }
+  }   
+
 });
 
 
