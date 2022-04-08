b403b30281b5c4950b505b478a5d0f99fd077234
Anjali Srivastava <anjalisriv023@gmail.com>
2022-04-08 23:50:42 +0530

    assignment solution

--- src/routes/route.js
+++ src/routes/route.js
@@ -26,7 +26,7 @@ router.get('/missing1', function(req,res){
   let b=[33,34,35,36,37,38,39,40,42];
   let sum1=0;
   let sum2=0;
-  for(let i=0; i<b.length; i+git add +){  
+  for(let i=0; i<b.length; i++){  
     sum1=sum1+b[i]; 
 }
 for (let i=b[0];i<=b[(b.length)-1];i++)       //calculate value of i to find total sum
