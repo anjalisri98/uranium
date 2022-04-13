b4d638574a3c0ce9ca0fe96baa62d9c7cbb5db29
Anjali Srivastava <anjalisriv023@gmail.com>
2022-04-13 17:10:18 +0530

    assignmet solution

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,20 +1,20 @@
 const mongoose = require('mongoose');
 
-const bookSchema = new mongoose.Schema( {
-    bookName: String, 
-    authorName: String, 
-    tags: [String],
+// const bookSchema = new mongoose.Schema( {
+//     bookName: String, 
+//     authorName: String, 
+//     tags: [String],
     
-    isPublished: Boolean,
-    prices: {
-        indianPrice: String,
-        europePrice: String,
-    },
-    sales: {type: Number, default: 10}
-}, { timestamps: true });
+//     isPublished: Boolean,
+//     prices: {
+//         indianPrice: String,
+//         europePrice: String,
+//     },
+//     sales: {type: Number, default: 10}
+// }, { timestamps: true });
 
 
-module.exports = mongoose.model('Book', bookSchema) //users
+// module.exports = mongoose.model('Book', bookSchema) //users
 
 //Validation:
 //require:true
@@ -29,3 +29,26 @@ module.exports = mongoose.model('Book', bookSchema) //users
 // Object
 // ObjectId
 // Buffer - not cover
+// Create a books collection in your DB ( using bookModel with following fields)- bookName( mandatory field), price containing Indian and european price, year ( should be 2021 if no year is provided) , tags array, authorName, totalPages , stockAvailable ( true false) 
+
+const bookSchema = new mongoose.Schema( {
+    bookName:{
+        type:String,
+        required:true
+    },
+    price:{
+        indianPrice: String,
+        europePrice: String,
+    },
+    year:{
+        type:String,
+        default:2021
+    },
+    tags:[String],
+    authorname:String,
+    totalPages: Number,
+    stockAvailable:Boolean //true or false
+},{ timestamps: true })
+
+module.exports = mongoose.model('getBooks' ,bookSchema ) //getbooks
+
